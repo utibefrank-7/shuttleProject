c36a3f8407fe4850f9f3a086f82788882531b25c
fran utibe joseph <majesticfrank7@gmail.com>
2026-03-19 17:21:31 +0100

    re-structuring

--- shuttle/shuttle/settings.py
+++ shuttle/shuttle/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-15x93an@)iwcmx+1=rv0mo*e2n#_sr$2#$2*inr8*14nrfm^pc
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     "django_extensions",
     "tailwind",
     "theme",
+    
 ]
 
 MIDDLEWARE = [
@@ -51,6 +52,8 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
+
 ]
 TAILWIND_APP_NAME ="theme"
 
@@ -133,6 +136,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
