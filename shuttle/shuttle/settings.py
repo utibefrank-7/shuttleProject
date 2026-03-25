f7c2f55a85f576da1aa257cec3edf44e3ebf897e
fran utibe joseph <majesticfrank7@gmail.com>
2026-03-25 06:14:29 +0100

    setting db again

--- shuttle/shuttle/settings.py
+++ shuttle/shuttle/settings.py
@@ -13,6 +13,8 @@ import os
 import dj_database_url
 from pathlib import Path
 
+from django.contrib import staticfiles
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
