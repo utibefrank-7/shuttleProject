ceb89967c26ea73b20a546946e614b9614b666bb
fran utibe joseph <majesticfrank7@gmail.com>
2026-03-26 20:16:46 +0100

    Added pillow for images

--- shuttle/shuttle/settings.py
+++ shuttle/shuttle/settings.py
@@ -98,8 +98,9 @@ WSGI_APPLICATION = "shuttle.wsgi.application"
 
 EMAIL_BACKEND ='django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST ='smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
+EMAIL_PORT = 465
+EMAIL_USE_TLS = False
+EMAIL_USE_SSL = True
 EMAIL_HOST_USER =os.getenv('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 DEFAULT_FROM_EMAIL = os.getenv('EMAIL_HOST_USER')
