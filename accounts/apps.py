5d2d522b0bdc9b4b019c5959bab3555d9f15690a
fran utibe joseph <majesticfrank7@gmail.com>
2026-04-06 08:31:16 +0100

    creating admin

--- accounts/apps.py
+++ accounts/apps.py
@@ -1,10 +1,12 @@
 from django.apps import AppConfig
-
+import os
 
 class AccountsConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "accounts"
 
     def ready(self):
-        from .create_admin import create_admin
-        create_admin()
+        # Only run in production and when explicitly enabled
+        if os.getenv("DJANGO_SUPERUSER_USERNAME"):
+            from .create_admin import create_admin
+            create_admin()
